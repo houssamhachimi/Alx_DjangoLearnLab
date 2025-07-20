a378eea07c6dc6f2ffbbef2660dddbffa204a4cd
Houssam Hachimi <houssamhachimi84@gmail.com>
2025-07-20 21:23:20 +0100

    n

--- Introduction_to_Django/LibraryProject/bookshelf/models.py
+++ Introduction_to_Django/LibraryProject/bookshelf/models.py
@@ -1,13 +1,10 @@
 from django.db import models
 
-# Create your models here.
-
-from django.db import models
-
 class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=100)
-    publication_year = models.IntegerField()
+    publication_year = models.PositiveIntegerField()
 
     def __str__(self):
-        return f"{self.title} by {self.author} ({self.publication_year})"
+        return f"{self.title} by {self.author}"
+
