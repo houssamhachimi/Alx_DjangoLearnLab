396d0acc1dd58dace0a70f901e7ae06722cb9576
Houssam Hachimi <houssamhachimi84@gmail.com>
2025-08-03 15:21:52 +0100

    q

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,8 +1,13 @@
-from django.shortcuts import render  # required even if not used, just in case
+from django.shortcuts import render
+from .models import Book, Library  # import Library for later
+
+def list_books(request):
+    books = Book.objects.all()  # Simple queryset, no extras
+    return render(request, 'relationship_app/list_books.html', {'books': books})
+
 from django.views.generic.detail import DetailView
-from .models import Book, Library  # ✅ matches "from .models import Library"
 
 class LibraryDetailView(DetailView):
     model = Library
-    template_name = 'relationship_app/library_detail.html'  # ✅ matches expected string
-    context_object_name = 'library'  # ✅ this ensures {{ library }} in template works
+    template_name = 'relationship_app/library_detail.html'
+    context_object_name = 'library'
