fd5e92718d1229c689bb2c76d755d27287622c27
Houssam Hachimi <houssamhachimi84@gmail.com>
2025-08-03 14:16:24 +0100

    m

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
-from .models import Book
+from django.views.generic.detail import DetailView
+from .models import Library  # ✅ Required for the check
 
 def list_books(request):
-    books = Book.objects.all()  # Required exact line for the check
+    books = Book.objects.all()
     return render(request, 'relationship_app/list_books.html', {'books': books})
+
+class LibraryDetailView(DetailView):
+    model = Library
+    template_name = 'relationship_app/library_detail.html'  # ✅ Required string
+    context_object_name = 'library'  # ✅ Required string
