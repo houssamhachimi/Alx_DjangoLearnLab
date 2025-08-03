0979d0e8e61d3d14acb55ff770e76b23b3c49b1d
Houssam Hachimi <houssamhachimi84@gmail.com>
2025-08-03 14:46:05 +0100

    r

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,9 +1,23 @@
 from django.contrib.auth.decorators import user_passes_test
 from django.shortcuts import render
 
-def check_member(user):
+def is_admin(user):
+    return hasattr(user, 'userprofile') and user.userprofile.role == 'Admin'
+
+def is_librarian(user):
+    return hasattr(user, 'userprofile') and user.userprofile.role == 'Librarian'
+
+def is_member(user):
     return hasattr(user, 'userprofile') and user.userprofile.role == 'Member'
 
-@user_passes_test(check_member)
+@user_passes_test(is_admin)
+def admin_view(request):
+    return render(request, 'relationship_app/admin_view.html')
+
+@user_passes_test(is_librarian)
+def librarian_view(request):
+    return render(request, 'relationship_app/librarian_view.html')
+
+@user_passes_test(is_member)
 def member_view(request):
     return render(request, 'relationship_app/member_view.html')
