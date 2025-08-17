201526e5eceedd95e8932ed99a07f6d6fe6d4ec6
Houssam Hachimi <houssamhachimi84@gmail.com>
2025-08-17 11:08:53 +0100

    nikes

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,36 +1,15 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import login, authenticate, logout
-from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
-from django.contrib.auth.decorators import login_required
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
 
-
-# Register view
-def register_view(request):
+# Register view (function-based)
+def register(request):
     if request.method == "POST":
         form = UserCreationForm(request.POST)
         if form.is_valid():
             user = form.save()
-            login(request, user)  # Auto login after registration
-            return redirect("list_books")  # Redirect to books page
-    else:
-        form = UserCreationForm()
-    return render(request, "relationship_app/register.html", {"form": form})
-
-
-# Login view
-def login_view(request):
-    if request.method == "POST":
-        form = AuthenticationForm(request, data=request.POST)
-        if form.is_valid():
-            user = form.get_user()
             login(request, user)
             return redirect("list_books")
     else:
-        form = AuthenticationForm()
-    return render(request, "relationship_app/login.html", {"form": form})
-
-
-# Logout view
-def logout_view(request):
-    logout(request)
-    return render(request, "relationship_app/logout.html")
+        form = UserCreationForm()
+    return render(request, "relationship_app/register.html", {"form": form})
